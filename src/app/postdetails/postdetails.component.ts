8033a741a9d3208f49f535d0d7d0336566eef91a
Siddharth Singh <siddharths@maveric-systems.com>
2022-03-24 16:29:00 +0530

    post details added

--- src/app/postdetails/postdetails.component.ts
+++ src/app/postdetails/postdetails.component.ts
@@ -9,13 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class PostdetailsComponent implements OnInit {
 
   post:any;
-  postedBy:any={}
+  postedBy:any={};
+  comments:any;
   constructor(private http : HttpClient) {
     let url= "http://localhost:3010/posts/2";
     this.http.get(url).subscribe((response:any)=>{
       console.log(response)
       this.post=response
       this.postedBy=response.postedBy;
+    })
+
+    let url1="http://localhost:8000/posts/2/comments"
+    this.http.get(url1).subscribe((response:any)=>{
+      console.log(response)
+      this.comments=response
+
     })
   }
 
