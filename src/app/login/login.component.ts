ba8f12d9f437ebf46f339d6d7f399aa2bd46f90d
Siddharth Singh <siddharths@maveric-systems.com>
2022-03-24 15:07:54 +0530

    login function started

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -22,12 +22,37 @@ export class LoginComponent implements OnInit {
       email:this.user.email,
       password:this.user.password
     }
+
+    let body1={
+      "firstName": "Fourth",
+      "lastName": "Test",
+      "middleName": "User",
+      "phoneNumber": "7894561230",
+      "email": "fourthyyyy@example.com",
+      "address": "pune",
+      "dateOfBirth": "2022-03-21",
+      "employeeNumber": "564",
+      "bloodGroup": "A_POS",
+      "gender": "MALE",
+      "martialStatus":"Bachelor",
+      "password":"789"
+    }
+
+    let headers =new HttpHeaders();
+    headers.append('Access-Control-Allow-Headers', 'Content-Type');
+    headers.append('Access-Control-Allow-Methods', 'POST');
+    headers.append('Access-Control-Allow-Origin', '*');
+
+
     console.log("Hello")
-    // let url="http://localhost:8000/auth/login";
-    let url ="http://localhost:8000/posts";
+    console.log({...body});
+    let url="http://localhost:8000/auth/login";
+    // let url ="http://localhost:8000/posts";
     // let url="https://apifromashu.herokuapp.com/api/login";
 
-    this.http.get(url).subscribe((response:any)=>{
+    this.http.post(url,body,{
+      headers:headers
+    }).subscribe((response:any)=>{
 
       console.log(response)
     },(error)=>alert('Login erro'))
